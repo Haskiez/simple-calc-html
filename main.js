28fe0598e77e0a29eefb730e4f295dc146120795
Nate Back <nkback@gmail.com>
2018-02-01 20:09:16 -0700

    Final commit

--- main.js
+++ main.js
@@ -28,3 +28,39 @@ function multiplyStuff(){
        document.getElementById("answer").value = valueA * valueB;
    }
 }
+function addStuff(){
+    var valueA =  document.getElementById("a").value;
+    var valueB = document.getElementById("b").value;
+ 
+    if(valueA === "" || valueB === "" || valueA === " " || valueB === " ")
+    {
+        window.alert("You have to submit a value");
+    }
+    else{
+        document.getElementById("answer").value = parseFloat(valueA) + parseFloat(valueB);
+    }
+ }
+ function subtractStuff(){
+    var valueA =  document.getElementById("a").value;
+    var valueB = document.getElementById("b").value;
+ 
+    if(valueA === "" || valueB === "" || valueA === " " || valueB === " ")
+    {
+        window.alert("You have to submit a value");
+    }
+    else{
+        document.getElementById("answer").value = valueA - valueB;
+    }
+ }
+ function divideStuff(){
+    var valueA =  document.getElementById("a").value;
+    var valueB = document.getElementById("b").value;
+ 
+    if(valueA === "" || valueB === "" || valueA === " " || valueB === " ")
+    {
+        window.alert("You have to submit a value");
+    }
+    else{
+        document.getElementById("answer").value = valueA / valueB;
+    }
+ }
